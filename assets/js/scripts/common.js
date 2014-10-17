6a3cdf5881eef6b42ad217f5463646ae0d7f10c4
djquinn <djquinn@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-17 20:59:08 +0000

    fixed problem with the way rewrite array is interpreted before being registered (again)
    git-svn-id: https://plugins.svn.wordpress.org/wp-extend/tags/1.1.0@1009271 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ assets/js/scripts/common.js
@@ -0,0 +1,20 @@
+jQuery(document).ready(function($) {
+
+	window.wpx = {
+
+		init: function() {
+
+			this.universalSeparator = '!wpx!';
+			this.bindEvents();
+
+		},
+
+		bindEvents: function() {
+
+		}
+
+	};
+	
+	wpx.init();
+
+});
